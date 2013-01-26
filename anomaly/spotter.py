4ba38a33636ada927621f52ddb15dc77906278be
Michael Mulich <michael.mulich@gmail.com>
2013-01-25 23:54:07 -0500

    Spotter consumer/producer with logging

--- anomaly/spotter.py
+++ anomaly/spotter.py
@@ -4,8 +4,11 @@ A consumer of raw job data. The output of this consumer is
 a more procise and trackable message that is published to the
 anomaly exchange.
 """
+import argparse
+import logging
 import json
 import jsonpickle
+from logging.config import fileConfig
 from pika import BasicProperties
 from pika.adapters import BlockingConnection
 
@@ -15,6 +18,8 @@ from .utils import get_routing_key
 
 QUEUE = 'anomaly-spotted'
 EXCHANGE = 'anomaly-analysis'
+
+logger = logging.getLogger('anomaly')
 
 
 def consumer(consuming_channel, method, header, body):
@@ -41,6 +46,8 @@ def consumer(consuming_channel, method, header, body):
                           routing_key=routing_key,
                           properties=properties,
                           body=message)
+    logger.debug("Sent message to '{0}' with {1!r}".format(routing_key,
+                                                            message))
     connection.close()
 
     # Acknowledge message receipt
@@ -49,6 +56,15 @@ def consumer(consuming_channel, method, header, body):
 
 def main(argv=None):
     """Main logic hit by the commandline invocation."""
+    parser = argparse.ArgumentParser(__doc__)
+    parser.add_argument('-c', '--config',
+                        help="path to the configuration file")
+    args = parser.parse_args(argv)
+    if args.config is not None:
+        fileConfig(args.config)
+        logger.info("Logging initialized")
+
+    # Queue initialization
     connection = BlockingConnection()
     channel = connection.channel()
     channel.queue_declare(queue=QUEUE, durable=True, exclusive=False)
